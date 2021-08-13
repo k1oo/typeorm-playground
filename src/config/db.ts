8dc46eb165332233eb868155ffac28f1e034428d
k1oo <kbaek1202@gmail.com>
2021-08-13 09:10:37 +0900

    feat: db 연결 시 posts 테이블 데이터 세팅

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
 import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 import * as fs from 'fs';
+import * as randomstring from 'randomstring';
 
 export class DB {
   private connection: Connection;
@@ -11,6 +12,7 @@ export class DB {
     try {
       this.connection = await createConnection(this.connectionOptions);
       await this.resetTables();
+      await this.settingPostTables();
     } catch (e) {
       console.error(e);
       throw new Error(e);
@@ -22,12 +24,24 @@ export class DB {
       fs.readdir('src/entities', (err, fileList) => {
         fileList.forEach(async file => {
           const entity = file.replace(/\..*/g, '') + 's';
-          await this.connection.query(`DELETE FROM ${entity.toLowerCase()}`);
+          await this.connection.query(`TRUNCATE TABLE ${entity.toLowerCase()} RESTART IDENTITY;`);
         });
       });
     } catch (e) {
       console.error(`resetTables Error: `, e);
       throw new Error(e);
+    }
+  }
+
+  async settingPostTables() {
+    try {
+      for (let i = 0; i < 100; i++) {
+        await this.connection.query(
+          `INSERT INTO "posts" ("title", "body") 
+          VALUES ('${randomstring.generate(10)}', '${randomstring.generate(30)}')`);
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
 }
